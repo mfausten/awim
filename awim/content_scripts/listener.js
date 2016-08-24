b71db12f8c9afae55671b62db991825ac7fe3fe0
Michael Fausten <m.fausten@anymotion.de>
2016-08-24 12:13:19 +0200

    ugly working version

--- awim/content_scripts/listener.js
+++ awim/content_scripts/listener.js
@@ -7,11 +7,68 @@
   * to the content_script in the found tab(s).
   *
   **/
-browser.browserAction.onClicked.addListener(function(tab) {
+/*browser.browserAction.onClicked.addListener(function(tab) {
   browser.tabs.query({}, function(result){
     result.forEach(function(value, index){
-      if(value.url.startsWith('https://www.youtube.com'))
-        browser.tabs.sendMessage(value.id, {});
+		console.log('inside listener');
+		
+		
+      if(value.url.startsWith('https://www.youtube.com')){
+	  browser.tabs.sendMessage(value.id, {});}
+	
+	
+	  if(value.url.startsWith('https://play.spotify.com')){
+		  console.table(value);
+		  console.log('found spotify tab');
+		//browser.tabs.sendMessage(value.id, {});  
+		
+		chrome.tabs.executeScript(null, { file: "/content_scripts/stopSpotify.js" });
+		
+	  }
+		  
+        
     });
   });
+});*/
+
+document.addEventListener("click", function(e) {
+	
+	console.log('inside event listener');
+	
+	browser.tabs.query({}, function(result){
+	  
+        console.log(result);
+		console.log('iterating...');
+		
+        result.forEach(function(value, index){
+          /*if(value.url.startsWith('https://www.youtube.com')){
+            console.log('found youtube tab');
+            console.log(value);
+            console.log('execute script');
+            browser.tabs.executeScript(value.id, { file: "/content_scripts/stop.js" }, function(result){console.log('received result!');console.log(result);})
+            console.log('send message')
+            browser.tabs.sendMessage(value.id, {});
+            console.log('done');
+          }*/
+		  
+		  if(value.url.startsWith('https://play.spotify.com')) {
+			
+			console.log('found spotify tab');
+			
+			//browser.tabs.executeScript(value.id, { file: "/content_scripts/stopSpotify.js" }, function(result) { console.log('result!'); console.table(result); });
+			
+			console.log('tab id: ' + value.id);
+			
+			chrome.tabs.executeScript(value.id, {
+				file: "/content_scripts/stopSpotify.js"
+			}, function(result) { console.log(result); });
+			
+			
+			
+			chrome.tabs.sendMessage(value.id, {beastURL: ''}, null, function(response) { console.log(response); });
+			
+		  }
+		  
+        });
+      });
 });
